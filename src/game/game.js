79d82033f8d6567d0a3a4db8a81cc2b72b38f536
Ian E Brons <ianebrons@gmail.com>
2020-07-02 11:04:59 -0400

    fixing stuff

--- src/game/game.js
+++ src/game/game.js
@@ -1,10 +1,6 @@
 import * as settings from './config.js'
-import { Map } from './map.js'
-import { Faction } from './faction.js'
-
 export class Game {
     constructor() {
-        this.map = new Map(settings.map_width, settings.map_height)
-        this.map.load_test()
+        console.log('Creating game')
     }
 }
