dac17a55ed7dd14d29f4f1e3af5a04db0ff7bb87
Ian Brons <ianebrons@gmail.com>
2020-10-13 12:55:32 -0400

    client side looking clean

--- src/client/client.js
+++ src/client/client.js
@@ -1,32 +1,13 @@
 import io from 'socket.io-client'
 import map from '../shared/map.js'
 import { Login } from './login.js'
+import { Display } from './display.js'
 
 const socket = io()
-let canvas = document.getElementById('canvas')
-let c = canvas.getContext('2d')
-c.imageSmoothingEnabled = false;
-let data = {}
-
-let map_image = new Image()
-map_image.src = "map.png"
-
-let default_draw = () => {
-    // draw_map(c)
-    canvas.style.width = 200 * 10 + "px"
-    canvas.style.height = 130 * 10 + "px"
-    let r = window.devicePixelRatio 
-    let h = window.innerHeight
-    let w = window.innerHeight * 200 / 130
-    c.drawImage(map_image, 0, 0)
-}
-
-window.onresize = () => default_draw()
-
-socket.on('update', (d) => {
-	default_draw()
-})
-
-let login = new Login(socket, () => {
-    document.getElementById("game").hidden = false
+const display = new Display()
+const login = new Login(socket, () => {
+        document.getElementById("game").hidden = false
+        socket.on('update', (data) => {
+                display.draw_map()
+        })
 })
--- src/client/display.js
+++ src/client/display.js
@@ -1,9 +1,24 @@
 export class Display {
         constructor() {
-                this.canvas = document.getElementById('canvas')
+                this.div = document.getElementById("game")
+                this.canvas = document.getElementById("canvas")
                 this.c = canvas.getContext('2d')
                 this.c.imageSmoothingEnabled = false;
                 this.map_image = new Image()
                 this.map_image.src = "map.png"
+
+                this.scale = 15
+
+                window.onresize = () => {
+                        this.draw_map()
+                }
+        }
+        fit_canvas() {
+                canvas.style.width = this.map_image.width * this.scale + "px"
+                canvas.style.height = this.map_image.height * this.scale + "px"
+        }
+        draw_map() {
+                this.fit_canvas()
+                this.c.drawImage(this.map_image, 0, 0)
         }
 }
