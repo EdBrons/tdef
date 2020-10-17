17331e105320a5c9a568ab5855787fd3a619112d
Ian Brons <ianebrons@gmail.com>
2020-10-17 14:32:17 -0400

    added keyboard movement

--- src/client/display.js
+++ src/client/display.js
@@ -12,8 +12,19 @@ class Background extends PIXI.Sprite {
 class Camera extends PIXI.Container {
 		constructor() {
 				super()
-				this.zoom = 5
+				this.zoom = 10
 				this.scale.set(this.zoom)
+		}
+		move_by(x, y) {
+				this.x -= x
+				this.y -= y
+				this.bound()
+		}
+		bound() {
+				this.x = Math.min(this.x, 0)
+				this.y = Math.min(this.y, 0)
+				this.x = Math.max(window.innerWidth - this.width, this.x)
+				this.y = Math.max(window.innerHeight - this.height, this.y)
 		}
 }
 
@@ -21,6 +32,7 @@ export class Display {
 		constructor() {
 				PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 				this.app = new PIXI.Application()
+				this.keys = new KeyboardInput()
 				document.body.appendChild(this.app.view)
 				LOADER.add('map.png').load(() => this.on_load())
 		}
@@ -31,11 +43,46 @@ export class Display {
 				this.background = new Background()
 				this.camera.addChild(this.background)
 				this.app.stage.addChild(this.camera)
+
+				// keyboard movement
+				this.keys.add_cb(38, () => this.camera.move_by(0, -1))
+				this.keys.add_cb(37, () => this.camera.move_by(-1, 0))
+				this.keys.add_cb(40, () => this.camera.move_by(0, 1))
+				this.keys.add_cb(39, () => this.camera.move_by(1, 0))
 		}
 		on_resize() {
 				this.resize()
 		}
 		resize() {
 				this.app.renderer.resize(window.innerWidth, window.innerHeight)
+		}
+}
+
+class KeyboardInput {
+		constructor() {
+				this.keys_down = {}
+				this.cbs = {}
+				// 30 times a second
+				this.delay = 1000 / 30
+				addEventListener("keydown", (e) => {this.keys_down[e.keyCode] = true})
+				addEventListener("keyup", (e) => {this.keys_down[e.keyCode] = false})
+				this._loop_helper()
+		}
+		add_cb(k, f) {
+				if (!this.cbs[k]) {
+						this.cbs[k] = []
+				}
+				this.cbs[k].push(f)
+		}
+		_loop_helper() {
+				this._loop()
+				setTimeout(() => this._loop_helper(), this.delay)
+		}
+		_loop() {
+				for (const key in this.keys_down) {
+						if (this.keys_down[key] && this.cbs[key] != undefined) {
+								for (const f of this.cbs[key]) f()
+						}
+				}
 		}
 }
